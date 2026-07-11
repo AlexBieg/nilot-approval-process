312c24218974f3d1d8f1f0c72b67e59c2af02963
Alex Bieg <alexbieg95@gmail.com>
2026-07-11 15:11:25 -0700

    Set up gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
+  site: 'https://alexbieg.github.io',
+  base: '/nilot-approval-process',
   build: {
     assets: 'assets',
   },
